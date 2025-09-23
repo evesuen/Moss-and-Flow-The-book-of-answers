ddac52c993b05c36d89ca8ae66a63eddc3592551
evesuen <evesuen@gmail.com>
2025-09-23 12:02:41 -0400

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
+    base: '/Interactive-Tarot-Card-Experience/', // 替换为你的GitHub仓库名
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
